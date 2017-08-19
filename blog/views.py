0b842bbcc4511a011e9f4a6175963c884a3a5d24
compilepeace <compilepeace@gmail.com>
2017-08-19 07:14:56 +0530

    modifies blog/urls.py for sys_info_page

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+
 # Create your views here.
 def post_list(request):
 	return render(request, 'blog/post_list.html', {})
+
+def sys_info_page(request):
+	return render(request, 'blog/sys_info_page.html', {})
